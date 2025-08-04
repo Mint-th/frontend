3f6097120764cdd3adb81dbf8cb398660abf59b8
Mint-th <1x909kohdyi@gmail.com>
2025-08-04 08:17:50 +0700

    first commit

--- app/conponents/Navigation.js
+++ app/conponents/Navigation.js
@@ -35,6 +35,8 @@ export default function Navbar() {
       <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
         <button className="btn btn-outline-warning" type="submit">Search</button>
+        <button className="btn btn-outline-warning" type="submit">Login</button>
+        <button className="btn btn-outline-warning" type="submit">สมัคร</button>
       </form>
     </div>
   </div>
